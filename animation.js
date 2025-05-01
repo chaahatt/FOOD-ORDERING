49134abf12699010258e7bdae1ef06409f7ae7fb
manavvv31 <manavmeet48@gmail.com>
2025-05-01 16:44:08 +0530

    Update animation.js

--- animation.js
+++ animation.js
@@ -2,3 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const ease = "power4.inOut";
 
     document.querySelectorAll("a").forEach((link) => {
+ link.addEventListener("click", (event) => {
+            event.preventDefault();
+            const href = link.getAttribute("href");
