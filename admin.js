c9810fb3fa613ed6511404cfa18729bb24572f76
ARSHDEEP SINGH GROVER <168627796+arshgrover02@users.noreply.github.com>
2025-05-01 16:50:08 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,16 +1,5 @@
-if (!localStorage.getItem('orders')) {
-      const defaultOrders = [
-        {
-          orderId: "12345",
-          selectedTimeSlot: "12:00 PM - 1:00 PM",
-          date: "2025-04-15",
-          status: "Order Confirmed",
-          items: [
-            { id: 1, name: "Pepperoni Pizza", quantity: 2, price: 20 },
-            { id: 2, name: "Chicken Cheese Pizza", quantity: 1, price: 25 }
-          ]
-        }
-      ];
-      localStorage.setItem('orders', JSON.stringify(defaultOrders));
-    }
-  };
+Calculating the total price
+      const total = order.items.reduce((sum, item) => {
+        return sum + (item.price * item.quantity);
+      }, 0);
+
